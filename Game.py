75224532dda850f0de38dd3b1408480734cea915
G00405014 <G00405014@atu.ie>
2023-07-28 10:07:28 +0100

    Firts commit

---
+++ Game.py
@@ -0,0 +1,16 @@
+print("Welcome to our computer game")
+
+playing = input("Do you want to play the game ? ")
+
+if playing != "yes":
+    quit() 
+
+print("okay lets play the game")
+
+answer = input("what is yout first name?")
+
+if answer!= "Emeka":
+    quit()
+    
+if answer == "Emeka": 
+    print("You got the question correct")
